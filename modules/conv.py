4968059ccf6663c3b9adf652ac3f575e46e55947
Schalmanoff <yaribakal@gmail.com>
2026-08-03 10:50:08 +0300

    made cnn

--- modules/conv.py
+++ modules/conv.py
@@ -1,6 +1,7 @@
 from scipy import signal
 from .layer import Layer
 import numpy as np
+from numpy.lib.stride_tricks import sliding_window_view
 
 class Conv2D(Layer):
     def __init__(self, in_channels, out_channels, kernel_size=3, padding=1):
@@ -9,68 +10,79 @@ class Conv2D(Layer):
         fan_in = kernel_size*kernel_size * in_channels
         std = np.sqrt(2.0/fan_in)
 
+        self.kernel_size=kernel_size
         self.padding = padding
 
-        self.kernel = np.random.randn(in_channels, out_channels, kernel_size, kernel_size) * std
+        self.kernel = (
+            np.random.randn(in_channels, out_channels, kernel_size, kernel_size)
+            * std
+        )
         self.bias = np.zeros((out_channels, 1, 1))
 
     def forward(self, prev):
         self.prev_layer = prev if isinstance(prev, Layer) else None
         self.prev_x = prev.x if self.prev_layer is not None else prev
-        batches, _, prev_x_h, prev_x_w = self.prev_x.shape
 
-        in_channels, out_channels, _, _ = self.kernel.shape
+        prev_x_padded = np.pad(
+            self.prev_x,
+            (
+                (0, 0), (0, 0), (self.padding, self.padding), (self.padding, self.padding)
+            )
+        )
 
-        self.x = np.zeros((batches, out_channels, prev_x_h, prev_x_w))
+        self.windows = sliding_window_view(
+            prev_x_padded,
+            (self.kernel_size, self.kernel_size),
+            axis = (2, 3)
+        )
 
-        for b in range(batches):
-            for out_c in range(out_channels):
-                for in_c in range(in_channels):
-                    prev_x_padded = np.pad(
-                        self.prev_x[b, in_c], 
-                        self.padding
-                    )
-
-                    self.x[b, out_c] += signal.correlate2d(
-                        prev_x_padded,
-                        self.kernel[in_c, out_c],
-                        mode='valid',
-                    )
-
-        self.x += self.bias[None]
+        self.x = np.einsum(
+            "bchwij,coij->bohw",
+            self.windows,
+            self.kernel,
+            optimize=True
+        ) + self.bias[None]
 
         return self
 
     def backward(self, dx):
-        in_channels, out_channels, k_size, _ = self.kernel.shape
+        batch_size = dx.shape[0]
 
-        batches = dx.shape[0]
-        self.dkernel = np.zeros_like(self.kernel)
-        self.dbias = np.sum(dx, axis=(0, 2, 3)).reshape(out_channels, 1, 1) / batches
-
-        for b in range(batches):
-            for out_c in range(out_channels):
-                for in_c in range(in_channels):
-                    prev_x_padded = np.pad(
-                        self.prev_x[b, in_c], 
-                        self.padding
-                    )
-
-                    self.dkernel[in_c, out_c] += signal.correlate2d(
-                        prev_x_padded, dx[b, out_c], mode='valid'
-                    )
-
-        self.dkernel /= batches
+        self.dkernel = np.einsum(
+            "bchwij,bohw->coij",
+            self.windows,
+            dx,
+            optimize=True
+        ) / batch_size
+        self.dbias = np.sum(dx, axis=(0, 2, 3), keepdims=False)[:, None, None] / batch_size
 
         if not self.prev_layer: return
 
-        prev_dx = np.zeros_like(self.prev_layer.x)
-        for b in range(batches):
-            for out_c in range(out_channels):
-                for in_c in range(in_channels):
-                    prev_dx[b, in_c] += signal.convolve2d(
-                        dx[b, out_c], self.kernel[in_c, out_c], mode='same'
-                    )
+        k = self.kernel_size
+
+        dx_padded = np.pad(
+            dx,
+            (
+                (0, 0), (0, 0), (k-1, k-1), (k-1, k-1)
+            )
+        )
+
+        dx_windows = sliding_window_view(
+            dx_padded,
+            (k, k),
+            axis=(2, 3),
+        )
+
+        prev_dx = np.einsum(
+            "bohwij,coij->bchw",
+            dx_windows,
+            self.kernel[:, :, ::-1, ::-1],
+            optimize=True
+        )
+
+        p = self.padding
+        if p:
+            prev_dx = prev_dx[:, :, p:-p, p:-p]
 
         self.prev_layer.backward(prev_dx)
 
